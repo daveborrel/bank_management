f5ecf59457cf3aede1a3250940875a095d7bede0
daveborrel <daveborrel27@gmail.com>
2024-02-26 22:24:58 -0800

    added basic classes

---
+++ src/customer.cpp
@@ -0,0 +1,11 @@
+// my_class.cpp
+#include "customer.h" // header in local directory
+#include <iostream> // header in standard library
+
+using namespace N;
+using namespace std;
+
+double customer::getNumOfAccounts()
+{
+    cout << "Doing something!" << endl;
+}
---
+++ src/customer.h
@@ -0,0 +1,12 @@
+// my_class.h
+namespace N
+{
+    class customer
+    {
+    public:
+        double getNumOfAccounts();
+    private:
+        double numberOfAccounts;
+    };
+
+}
